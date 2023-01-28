9d5408e41edeb623c525a369d85d32ef88d67f2a
samanfathnazarian <saman.nazarian17@gmail.com>
2023-01-28 13:43:32 -0600

    Working with State & Events

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
@@ -9,11 +9,51 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
+class Users extends Component {
+  constructor() {
+    super();
+    // in class-based components there can only be 1 state which MUST be an object
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  toggleUsersHandler() {
+    // When a traditional function is called, 'this' refers to the global object (or undefined in strict mode) rather than the component instance.
+    // console.log('this Keyword: ', this);
+
+    // this.state.showUsers = false; //DOES NOT WORK!!
+    this.setState((curState) => {
+      // REMEMBER, states must always be an object
+      return { showUsers: !curState.showUsers }; //react does not overwrite, react merges the objects, (the opposite of setShowUsers() in functional components)
+    });
+  }
+
+  render() {
+    // can define helper const in render method
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>{this.state.showUsers ? 'Hide' : 'Show'} Users</button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+/*
 const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
+    setShowUsers((curState) => !curState); //react overwrites the old state, react doesn't merge it
   };
 
   const usersList = (
@@ -31,5 +71,6 @@ const Users = () => {
     </div>
   );
 };
+*/
 
 export default Users;
